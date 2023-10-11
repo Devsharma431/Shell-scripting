524d6cb08db63d90b08c18a38223a4801d4e7128
Devsharma431 <devpython345898@gmail.com>
2023-10-11 14:19:36 +0530

    read and arguments in bash

--- bestday.sh
+++ bestday.sh
@@ -1,6 +1,10 @@
-echo 'Good Morning Dev sharma'
+#!/bin/bash
+echo 'what is ur name'
+read name
+n=$(date)
+echo "Good Morning $name"
 sleep 1
-echo 'You looking good today'
+echo "You looking good $n"
 sleep 1
-echo "you have the best skills I've ever seen"
+echo "you have the best skills I've ever seen $name"
 sleep 1
